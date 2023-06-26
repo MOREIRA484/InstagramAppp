ed7ab0c3bae0ca29172c964c2553ae042c0eeb85
leonardo <leonardoazevedomoreiraa@gmail.com>
2023-06-26 12:15:29 -0300

    Initial Commit

---
+++ InstagramApp/InstagramAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramAppApp.swift
+//  InstagramApp
+//
+//  Created by leonardo Moreira on 26/06/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
